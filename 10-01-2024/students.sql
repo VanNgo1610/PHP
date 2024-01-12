b43a536e04cd3a86864f08de1b13fa4cfbf023bd
VanNgo1610 <vanthngo.csp@gmail.com>
2024-01-12 09:08:55 +0700

    create

---
+++ 10-01-2024/students.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `students` (
+                                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                                          `name` varchar(100) NOT NULL,
+                                          `email` varchar(50) NOT NULL,
+                                          PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET = latin1 AUTO_INCREMENT = 4;
+
+INSERT INTO `students` (`id`, `name`, `email`) VALUES
+                                                   (1, 'Jackie Ross', 'jackie.ross@example.com'),
+                                                   (2,'Logan Bennett', 'logan.bennett@example.com'),
+                                                   (3, 'Judy Fuller', 'judy.fuller@example.com');
